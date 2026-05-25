edc03280b5152bf24ff09af9a41514b7bd1e745f
Benjamin <benjamin.tran@bellevuecollege.edu>
2026-05-24 21:18:17 -0700

    config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
-  // Update this to match your actual GitHub repository name
-  basePath: "/lumberjackwins", 
+
+  basePath: "/lumberjackwins",
+  assetPrefix: "/lumberjackwins/",
 };
 
 export default nextConfig;
